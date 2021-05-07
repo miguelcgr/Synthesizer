c884f273779d21ca7db02776f1795bc808347277
miguel <miguelcalvoguti@gmail.com>
2021-05-07 17:51:50 +0200

    instrument change buttons not working

--- src/Components/Piano.js
+++ src/Components/Piano.js
@@ -1,47 +1,46 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import * as Tone from "tone";
- 
 
 const Piano = () => {
 
-   const phaser = new Tone.Phaser({
-        frequency: 15,
-        octaves: 5,
-         baseFrequency: 1000
-      }).toDestination();
-    
-   const phas = new Tone.FMSynth().connect(phaser);
 
-   const fatOsc = new Tone.FatOscillator("Ab3", "sawtooth", 40).toDestination()
+
+  const phaser = new Tone.Phaser({
+    frequency: 15,
+    octaves: 5,
+    baseFrequency: 1000,
+  }).toDestination();
+
+  const phas = new Tone.FMSynth().connect(phaser);
+
+  const fatOsc = new Tone.FatOscillator("Ab3", "sawtooth", 40).toDestination();
 
   const synth = new Tone.Synth().toDestination();
 
-
   const oscill = new Tone.Synth({
-    oscillator : {
-      type :'fmsquare',
-          modulationType : 'sawtooth',
-          modulationIndex : 3,
-          harmonicity: 3.4
+    oscillator: {
+      type: "fmsquare",
+      modulationType: "sawtooth",
+      modulationIndex: 3,
+      harmonicity: 3.4,
     },
-    envelope : {
-      attack : 0.001,
-          decay : 0.1,
-          sustain: 0.1,
-          release: 0.1
-    }
-  }).toDestination() 
+    envelope: {
+      attack: 0.001,
+      decay: 0.1,
+      sustain: 0.1,
+      release: 0.1,
+    },
+  }).toDestination();
 
+  const [pitch, setPitch] = useState(5);
 
+  const [instrument, setInstrument] = useState(synth);
 
-  const [pitch, setPitch] = useState(5)
-
-  const [instrument, setInstrument]= useState(synth)
-
-  const now = Tone.now()
+  
+  const now = Tone.now();
 
   const playNote = (event) => {
-    console.log('instrument', instrument.name)
+    console.log("instrument", instrument.name);
     let note = event.target.className;
 
     instrument.triggerAttack(`${note}${pitch}`, "8n");
@@ -52,62 +51,119 @@ const Piano = () => {
     instrument.triggerRelease(now);
   };
 
-
   return (
     <div>
-      
-      <div className='piano'>
-        <button id='white' onMouseUp={(event) => silenceNote(event)}  onMouseDown={(event) => playNote(event)} className="C">
+      <div className="piano">
+        <button
+          id="white"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="C"
+        >
           C
         </button>
 
-        <button id='black' onMouseUp={(event) => silenceNote(event)}  onMouseDown={(event) => playNote(event)} className="C#">
+        <button
+          id="black"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="C#"
+        >
           C#
         </button>
 
-        <button id='white' onMouseUp={(event) => silenceNote(event)} onMouseDown={(event) => playNote(event)} className="D">
+        <button
+          id="white"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="D"
+        >
           D
         </button>
-        <button id='black' onMouseUp={(event) => silenceNote(event)}  onMouseDown={(event) => playNote(event)} className="D#">
+        <button
+          id="black"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="D#"
+        >
           D#
         </button>
-        <button id='white' onMouseUp={(event) => silenceNote(event)} onMouseDown={(event) => playNote(event)} className="E">
+        <button
+          id="white"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="E"
+        >
           E
         </button>
-        <button id='white' onMouseUp={(event) => silenceNote(event)} onMouseDown={(event) => playNote(event)} className="F">
+        <button
+          id="white"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="F"
+        >
           F
         </button>
-        <button id='black' onMouseUp={(event) => silenceNote(event)}  onMouseDown={(event) => playNote(event)} className="F#">
+        <button
+          id="black"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="F#"
+        >
           F#
         </button>
-        <button id='white' onMouseUp={(event) => silenceNote(event)} onMouseDown={(event) => playNote(event)} className="G">
+        <button
+          id="white"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="G"
+        >
           G
         </button>
-        <button id='black' onMouseUp={(event) => silenceNote(event)}  onMouseDown={(event) => playNote(event)} className="G#">
+        <button
+          id="black"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="G#"
+        >
           G#
         </button>
-        <button id='white' onMouseUp={(event) => silenceNote(event)} onMouseDown={(event) => playNote(event)} className="A">
+        <button
+          id="white"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="A"
+        >
           A
         </button>
-        <button id='black' onMouseUp={(event) => silenceNote(event)}  onMouseDown={(event) => playNote(event)} className="A#">
+        <button
+          id="black"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="A#"
+        >
           A#
         </button>
-        <button id='white' onMouseUp={(event) => silenceNote(event)} onMouseDown={(event) => playNote(event)} className="B">
+        <button
+          id="white"
+          onMouseUp={(event) => silenceNote(event)}
+          onMouseDown={(event) => playNote(event)}
+          className="B"
+        >
           B
         </button>
       </div>
       <input
-       
-        onChange={event => setPitch(event.target.value)}
-        defaultValue='5'
+        onChange={(event) => setPitch(event.target.value)}
+        defaultValue="5"
         type="number"
         min="1"
         max="9"
       />
-      <button onCLick={()=> setInstrument(oscill)}>Oscillator</button>
-      <button onCLick={()=> setInstrument(fatOsc)}>Fat Oscillator</button>
-      <button onCLick={()=> setInstrument(synth)}>Synth</button>
-      <button onClick={()=> setInstrument(phas)}>Phaser</button> 
+      <button onCLick={() => setInstrument(oscill)}>Oscillator</button>
+      <button onCLick={() => setInstrument(fatOsc)}>Fat Oscillator</button>
+      <button onCLick={() => setInstrument(synth)}>Synth</button>
+      <button onClick={() => setInstrument(phas)}>Phaser</button>
     </div>
   );
 };
